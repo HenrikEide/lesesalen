b3bc207c743a976f6d796bc6ea8a05d4c34a786a
HenrikEide <Henrik225@gmail.com>
2023-04-02 19:58:56 +0200

    ugly solution

---
+++ _app/immutable/chunks/1.46e6d755.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.eb41be55.js";export{t as component};
--- build/_app/immutable/chunks/1.66760653.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.919e8d75.js";export{t as component};
