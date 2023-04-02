4ab3917e17d82a8eac1470a95a48a0dc6f1e365b
HenrikEide <Henrik225@gmail.com>
2023-04-02 22:27:49 +0200

    Updates

--- _app/immutable/chunks/1.2395fbf8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2370f92b.js";export{t as component};
---
+++ _app/immutable/chunks/1.8be82b12.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.4f52c6d4.js";export{t as component};
