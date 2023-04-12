cbac4e5695c86d4a4e63206fd33c0f3bf77aeb4a
HenrikEide <Henrik225@gmail.com>
2023-04-12 19:57:50 +0200

    lazy solution

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
 <div class="app">
 	<main>
 		<nav>
-			<a href="/lesesalen">Home</a>
-			<a href="/lesesalen/quiz">Quiz</a>
+			<a href="https://eiden.no/lesesalen/">Home</a>
+			<a href="https://eiden.no/lesesalen/quiz">Quiz</a>
 		</nav>
 		<section>
 			<slot />
