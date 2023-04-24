2d04752cf4926d5387eda735db5e7dee530dd00b
HenrikEide <Henrik225@gmail.com>
2023-04-24 21:20:31 +0200

    root test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
 <div class="app">
 	<main>
 		<nav>
-			<a href="https://eiden.no/lesesalen/">Home</a>
-			<a href="https://eiden.no/lesesalen/quiz">Quiz</a>
+			<a href="/lesesalen/">Home</a>
+			<a href="/lesesalen/quiz">Quiz</a>
 		</nav>
 		<section>
 			<slot />
@@ -46,7 +46,7 @@
 
 	nav a {
 		padding: 0.5rem;
-		color: #2F4F4F;
+		color: #2f4f4f;
 	}
 
 	section {
