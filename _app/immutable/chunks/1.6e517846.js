e732d7cef296d2c1bd604207b57d31ac9f478c60
HenrikEide <HenrikEide@users.noreply.github.com>
2023-04-27 18:26:13 +0000

    deploy: 92abca220db61dd5e3398da0326809b4f24b543b

--- _app/immutable/chunks/1.6e517846.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e75a6e9f.js";export{t as component};
---
+++ _app/immutable/chunks/3.811506d0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/quiz-page.svelte.a4eb9b0e.js";export{t as component};
